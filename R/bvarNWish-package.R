8afb57b344affd5433c6f31047478951ccff3492
Tomasz Woźniak <wozniak.tom@pm.me>
2025-04-14 11:36:38 +1000

    create the package skeleton

---
+++ R/bvarNWish-package.R
@@ -0,0 +1,7 @@
+#' @name bvarNWish-package
+#' @aliases bvarNWish-package bvarNWish
+#' @docType package
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib bvarNWish, .registration = TRUE
+#' @keywords internal
+'_PACKAGE'
