6f72ded8de8c9adb224ad24b63e5c414b5ac21b4
Tomasz Woźniak <wozniak.tom@pm.me>
2025-06-12 18:10:53 +1000

    documentation update #3
    description and references

--- R/bvarNWish-package.R
+++ R/bvarNWish-package.R
@@ -1,9 +1,8 @@
 #' 
-#' @title Bayesian Vector Autoregressions with Normal-Wishart Priors
+#' @title Bayesian Vector Autoregressions with Flexible Priors
 #' 
-#' @description Bayesian Vector Autoregressions with Normal-Wishart Priors and 
-#' the Matrix Generalized Inverse Gaussian Gibbs sampler.
-#' 
+#' @description Bayesian Vector Autoregressions with Normal-Matrix Generalised 
+#' Inverse Gaussian Priors.
 #' 
 #' @docType package
 #' @name bvarNWish-package
@@ -25,13 +24,16 @@
 #' Tomasz Woźniak \email{wozniak.tom@pm.me}
 #' 
 #' @references
+#' Chan (2020) Large Bayesian VARs: A Flexible Kronecker Error Covariance Structure,
+#' Journal of Business and Economic Statistics, 38(1), 68--79,
+#' <doi:10.1080/07350015.2018.1451336>.
 #' 
 #' Hamura, Irie, Sugasawa (2024) Gibbs Sampler for Matrix Generalized Inverse 
 #' Gaussian Distributions, Journal of Computational and Graphical Statistics,
 #' 33(2), 331--340, <doi:10.1080/10618600.2023.2258186>.
 #' 
-#' Chan (2020) Large Bayesian VARs: A Flexible Kronecker Error Covariance Structure,
-#' Journal of Business and Economic Statistics, 38(1), 68--79,
-#' <doi:10.1080/07350015.2018.1451336>.
+#' Thabane, Safiul Haq (2004) On the Matrix-Variate Generalized Hyperbolic 
+#' Distribution and Its Bayesian Applications, Statistics: A Journal of Theoretical 
+#' and Applied Statistics, 38(6), 511--526, <doi:10.1080/02331880412331319279>.
 #' 
 '_PACKAGE'
