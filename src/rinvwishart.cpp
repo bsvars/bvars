de4bdd8b667369946c0416476b3695e49fec38a3
Tomasz Woźniak <wozniak.tom@pm.me>
2026-05-19 18:33:27 +1000

    #5 simplify and sped up sampler

--- src/rinvwishart.cpp
+++ src/rinvwishart.cpp
@@ -5,10 +5,5 @@ using namespace arma;
 // [[Rcpp::depends(RcppArmadillo)]]
 // [[Rcpp::export]]
 arma::mat do_rinvwishart(const arma::mat& Psi, int df) {
-  
-  // Sample W ~ Wishart(df, inv(Psi))
-  arma::mat W = wishrnd(inv_sympd(Psi), df);
-  
-  // Return inverse: W^{-1} ~ InvWishart(df, Psi)
-  return inv_sympd(W);
+  return iwishrnd(Psi, df);
 }
