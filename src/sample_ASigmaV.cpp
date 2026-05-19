652517dd97455d2faf6a045f0ce16ef185e68739
Tomasz Woźniak <wozniak.tom@pm.me>
2026-05-19 18:32:00 +1000

    #5 correct and speed up sampleASigma

--- src/sample_ASigmaV.cpp
+++ src/sample_ASigmaV.cpp
@@ -80,18 +80,21 @@ arma::field<arma::mat> sample_ASigma(
   const mat     prior_A   = as<mat>(prior["A"]);
   const mat     prior_S   = as<mat>(prior["S"]);
   const double  prior_nu  = as<double>(prior["nu"]);
-  mat   Omega_inv         = diagmat(aux_Omega_diag_inv);
+  vec   Omega_diag_sqrt_inv = sqrt(aux_Omega_diag_inv);
   
-  mat   A_bar_tmp         = Y * Omega_inv * X.t() + prior_A * aux_V_inv;
-  mat   V_bar_inv         = X * Omega_inv * X.t() + aux_V_inv;
-  V_bar_inv               = 0.5 * (V_bar_inv + V_bar_inv.t());
-  mat   V_bar             = inv_sympd(V_bar_inv);
-  mat   A_bar             = A_bar_tmp * V_bar;
+  mat   Ys            = Y.each_row() / Omega_diag_sqrt_inv.t();
+  mat   Xs            = X.each_row() / Omega_diag_sqrt_inv.t();
+  mat   A_bar_tmp     = Ys * Xs.t() + prior_A * aux_V_inv;
+  mat   V_bar_inv     = Xs * Xs.t() + aux_V_inv;
+  V_bar_inv           = 0.5 * (V_bar_inv + V_bar_inv.t());
+  mat   V_bar         = inv_sympd(V_bar_inv);
+  mat   A_bar         = A_bar_tmp * V_bar;
   
   mat   S_bar         = prior_S;
-  S_bar              += Y * Omega_inv * Y.t();
+  S_bar              += Ys * Ys.t();
   S_bar              += prior_A * aux_V_inv * prior_A.t();
-  S_bar              += A_bar * V_bar_inv * A_bar.t();
+  S_bar              -= A_bar * V_bar_inv * A_bar.t();
+  S_bar               = 0.5 * (S_bar + S_bar.t());
   double nu_bar       = prior_nu + T;
   
   mat draw_Sigma      = iwishrnd(S_bar, nu_bar);
